8d0884ece7e3decf5afab996265b874d263003a6
Paul Lam <paul@quantisan.com>
2025-07-23 07:37:56 +0900

    feat: add Anthropic API key check

--- src/gremllm/renderer/state/system.cljs
+++ src/gremllm/renderer/state/system.cljs
@@ -5,3 +5,6 @@
 (defn encryption-available? [state]
   (get-in state (conj system-info-path :encryption-available?) false))
 
+(defn has-anthropic-api-key? [state]
+  (contains? (get-in state (conj system-info-path :secrets) {}) "anthropic-api-key"))
+
