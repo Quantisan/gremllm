d5253affd78ce9619c1b2f3f34515039c0da5c5f
Paul Lam <paul@quantisan.com>
2025-07-31 08:33:03 +0900

    refactor: Update topic action tests to use state path constant

--- test/gremllm/renderer/actions/topic_test.cljs
+++ test/gremllm/renderer/actions/topic_test.cljs
@@ -1,6 +1,7 @@
 (ns gremllm.renderer.actions.topic-test
   (:require [cljs.test :refer [deftest is]]
-            [gremllm.renderer.actions.topic :as topic]))
+            [gremllm.renderer.actions.topic :as topic]
+            [gremllm.renderer.state.topic :as topic-state]))
 
 (deftest set-topic-test
   (let [test-topic-js (clj->js {:id "t1"
@@ -9,7 +10,7 @@
         expected-normalized-topic {:id "t1"
                                    :name "Test Topic"
                                    :messages [{:id "m1" :type :user :content "Hi"}]}]
-    (is (= [[:effects/save [:topic] expected-normalized-topic]]
+    (is (= [[:effects/save topic-state/path expected-normalized-topic]]
            (topic/set-topic {} test-topic-js))
         "should convert JS object, normalize it, and return a save effect"))
   (is (nil? (topic/set-topic {} nil))
@@ -26,6 +27,6 @@
 
 (deftest start-new-topic-test
   (let [new-topic (topic/create-topic)]
-    (is (= [[:effects/save [:topic] new-topic]]
+    (is (= [[:effects/save topic-state/path new-topic]]
            (topic/start-new-topic {}))
         "should return a save effect with a new topic structure")))
