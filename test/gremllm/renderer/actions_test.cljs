7147789317a30f8673d7f0ef31cbcba4da1a5dc7
Paul Lam <paul@quantisan.com>
2025-08-16 08:16:13 +0900

    test: add test for nested promise actions with inner success value

--- test/gremllm/renderer/actions_test.cljs
+++ test/gremllm/renderer/actions_test.cljs
@@ -3,7 +3,6 @@
             [nexus.registry :as nxr]
             [gremllm.renderer.actions]))
 
-
 (deftest test-promise->actions-success
   (async done
     (let [store    (atom {})
@@ -33,4 +32,32 @@
       (js/setTimeout
         #(do (is (= expected (:error @store)))
              (done))
-        10))))
+        10)))
+
+ (deftest test-promise->actions-indirectly-nested-uses-inner-success
+   (async done
+     ;; Effect that dispatches a new promise (mimics topic.effects/*)
+     (nxr/register-effect! :test.effects/level-2
+       (fn [{:keys [dispatch]} _ & [opts]]
+         (dispatch
+           [[:effects/promise
+             {:promise    (js/Promise.resolve "inner")
+              :on-success (:on-success opts)}]])))
+
+     ;; Action that returns the above effect (mimics topic.actions/* -> topic.effects/*)
+     (nxr/register-action! :test.actions/chain-level-1
+       (fn [_ _]
+         ;; v is the outer success; inner promise's on-success should not see it
+         [[:test.effects/level-2
+           {:on-success [:effects/save [:result] [:promise/success-value]]}]]))
+
+    (let [store (atom {})]
+      (nxr/dispatch store {}
+         [[:effects/promise]
+          {:promise (js/Promise.resolve "outer")
+            :on-success [:test.actions/chain-level-1 [:promise/success-value]]}])
+      (js/setTimeout
+        #(do
+          (is (= "inner" (:result @store)))
+          (done))
+        10)))))
