2af6b0af98a9879d48a8a4752e7ce2863f78ebfc
Paul Lam <paul@quantisan.com>
2025-08-15 11:49:32 +0900

    refactor: Simplify test setup by moving effect registrations outside test cases

--- test/gremllm/renderer/actions_test.cljs
+++ test/gremllm/renderer/actions_test.cljs
@@ -1,24 +1,25 @@
 (ns gremllm.renderer.actions-test
   (:require [cljs.test :refer [deftest is async]]
             [nexus.registry :as nxr]
-            [gremllm.renderer.actions :as actions]))
+            [gremllm.renderer.actions]))
+
+(nxr/register-effect! :test/capture
+  (fn [_ store value]
+    (swap! store assoc :result value)))
+
+(nxr/register-effect! :test/capture-error
+  (fn [_ store value]
+    (swap! store assoc :error value)))
 
 (deftest test-promise->actions-success
   (async done
     (let [store (atom {})
           promise (js/Promise.resolve "success-value")]
-      
-      ;; Register a test effect to capture the result
-      (nxr/register-effect! :test/capture
-        (fn [_ store value]
-          (swap! store assoc :result value)))
-      
-      ;; Dispatch using the registry
       (nxr/dispatch store {}
-        [[:effects/promise 
+        [[:effects/promise
           {:promise promise
            :on-success [:test/capture]}]])
-      
+
       (js/setTimeout
         #(do (is (= "success-value" (:result @store)))
              (done))
@@ -28,16 +29,11 @@
   (async done
     (let [store (atom {})
           promise (js/Promise.reject "error-value")]
-      
-      (nxr/register-effect! :test/capture-error
-        (fn [_ store value]
-          (swap! store assoc :error value)))
-      
       (nxr/dispatch store {}
-        [[:effects/promise 
+        [[:effects/promise
           {:promise promise
            :on-error [:test/capture-error]}]])
-      
+
       (js/setTimeout
         #(do (is (= "error-value" (:error @store)))
              (done))
