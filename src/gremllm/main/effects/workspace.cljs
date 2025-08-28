5a595ac6b36129242e628693ee39c237ba78f5b2
Paul Lam <paul@quantisan.com>
2025-08-28 11:38:27 +0900

    chore: remove unused ns

--- src/gremllm/main/effects/workspace.cljs
+++
@@ -1,8 +0,0 @@
-(ns gremllm.main.effects.workspace
-  (:require [gremllm.main.io :as io]))
-
-(defn ls [workspace-dir]
-  (if-not (io/file-exists? workspace-dir)
-    []
-    (->> (io/read-dir workspace-dir)
-         vec)))
