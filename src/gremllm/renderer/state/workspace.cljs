78fae54cdf9ba107203f045c4f4bb63e69d69dd7
Paul Lam <paul@quantisan.com>
2025-09-28 05:48:27 +0900

    refactor: remove workspace-name-path and get-workspace-name function

--- src/gremllm/renderer/state/workspace.cljs
+++ src/gremllm/renderer/state/workspace.cljs
@@ -4,13 +4,8 @@
 
 (def loaded-path (conj workspace-path :loaded?))
 
-(def workspace-name-path (conj workspace-path :name))
-
 (defn get-workspace [state]
   (get-in state workspace-path))
-
-(defn get-workspace-name [state]
-  (get-in state workspace-name-path ""))
 
 (defn loaded? [state]
   (get-in state loaded-path false))
