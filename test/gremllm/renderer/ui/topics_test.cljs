e65288c5c9dc6022bb7a78b8c4cdfac1d211b52d
Paul Lam <paul@quantisan.com>
2025-10-04 06:41:42 +0900

    test: add topic UI tests for markers, rename input, dblclick

--- test/gremllm/renderer/ui/topics_test.cljs
+++ test/gremllm/renderer/ui/topics_test.cljs
@@ -29,4 +29,32 @@
         (is (= "page" (-> active-link lookup/attrs :aria-current))
             "Active topic should set aria-current to 'page'.")))))
 
+(deftest unsaved-and-active-markers-test
+  (let [props  {:workspace       {:name "Work"}
+                :topics-map      {"t1" {:id "t1" :name "Clean"}
+                                  "t2" {:id "t2" :name "Dirty" :unsaved? true}}
+                :active-topic-id "t2"}
+        hiccup (topics-ui/render-left-panel-content props)
+        links  (->> hiccup (lookup/select '[nav > ul > li > a]) rest)]
+    (is (= ["• Clean" "✓ Dirty *"] (map lookup/text links))
+        "Active topics show ✓, inactive show •, unsaved append *")))
+
+(deftest rename-mode-input-test
+  (let [props  {:workspace         {:name "Work"}
+                :topics-map        {"t1" {:id "t1" :name "Alpha"}}
+                :renaming-topic-id "t1"}
+        hiccup (topics-ui/render-left-panel-content props)
+        input  (lookup/select-one '[nav > ul > li > input] hiccup)]
+    (is (= "Alpha" (-> input lookup/attrs :default-value))
+        "Rename mode renders input with topic name as default-value")))
+
+(deftest double-click-rename-action-test
+  (let [props  {:workspace    {:name "Work"}
+                :topics-map   {"t1" {:id "t1" :name "Alpha"}}}
+        hiccup (topics-ui/render-left-panel-content props)
+        link   (->> hiccup (lookup/select '[nav > ul > li > a]) rest first)]
+    (is (= [:topic.actions/begin-rename "t1"]
+           (-> link lookup/attrs (get-in [:on :dblclick 1])))
+        "Double-click dispatches begin-rename action with topic ID")))
+
 
