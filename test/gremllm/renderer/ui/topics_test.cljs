ae7c2eb2a203ec838e00ca6abbe7af2e4cf23634
Paul Lam <paul@quantisan.com>
2025-10-31 10:19:06 +0900

    test: relax selector from 'nav > ul > li > a' to 'nav > ul > li a'

--- test/gremllm/renderer/ui/topics_test.cljs
+++ test/gremllm/renderer/ui/topics_test.cljs
@@ -35,7 +35,7 @@
                                   "t2" {:id "t2" :name "Dirty" :unsaved? true}}
                 :active-topic-id "t2"}
         hiccup (topics-ui/render-left-panel-content props)
-        links  (->> hiccup (lookup/select '[nav > ul > li > a]) rest)]
+        links  (->> hiccup (lookup/select '[nav > ul > li a]) rest)]
     (is (= ["• Clean" "✓ Dirty *"] (map lookup/text links))
         "Active topics show ✓, inactive show •, unsaved append *")))
 
@@ -52,7 +52,7 @@
   (let [props  {:workspace    {:name "Work"}
                 :topics-map   {"t1" {:id "t1" :name "Alpha"}}}
         hiccup (topics-ui/render-left-panel-content props)
-        link   (->> hiccup (lookup/select '[nav > ul > li > a]) rest first)]
+        link   (->> hiccup (lookup/select '[nav > ul > li a]) rest first)]
     (is (= [:topic.actions/begin-rename "t1"]
            (-> link lookup/attrs (get-in [:on :dblclick 1])))
         "Double-click dispatches begin-rename action with topic ID")))
