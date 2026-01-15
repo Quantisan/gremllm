8188ff3cb3504816f08692ebccad9dc154bb1ec7
Paul Lam <paul@quantisan.com>
2026-01-15 09:49:13 +0900

    Revert "feat (dev): main process starts Dataspex action logs too"
    This reverts commit 7b0b4ec8fd14a3362e4fc66ca990e752526114e9.

--- dev/gremllm/main/dev.cljs
+++ dev/gremllm/main/dev.cljs
@@ -1,7 +1,5 @@
 (ns gremllm.main.dev
-  (:require [nexus.action-log :as action-log]
-            [dataspex.core :as dataspex]
-            [gremllm.main.core :as core]
+  (:require [gremllm.main.core :as core]
             [gremllm.main.electron-runtime-test :as electron-runtime-test]))
 
 (defn main []
@@ -16,10 +14,6 @@
   (.config (js/require "@dotenvx/dotenvx") #js {:override true})
 
   (electron-runtime-test/test-safe-storage)
-
-  (action-log/inspect)
-  ;; connect to our JVM Dataspex server for remote viewing at http://localhost:7117/
-  (dataspex/connect-remote-inspector)
 
   ;; Delegate to the main entry point
   (core/main))
