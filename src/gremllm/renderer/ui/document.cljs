85b0c88e67d2aeca83ff60b991b5e4973ac60988
Paul Lam <paul@quantisan.com>
2026-03-05 07:20:08 +0900

    feat(document): add get-pending-diffs accessor and private render-diff-segments

--- src/gremllm/renderer/ui/document.cljs
+++ src/gremllm/renderer/ui/document.cljs
@@ -1,6 +1,14 @@
 (ns gremllm.renderer.ui.document
   (:require [gremllm.renderer.ui.markdown :as md]))
 
+(defn- render-diff-segments [segments]
+  (into [:div]
+        (mapv (fn [{:keys [type content old-text new-text]}]
+                (case type
+                  :text       [:span content]
+                  :diff-block [:div.diff-block [:del old-text] [:ins new-text]]))
+              segments)))
+
 (defn render-document [content]
   (if content
     [:article (md/markdown->hiccup content)]
