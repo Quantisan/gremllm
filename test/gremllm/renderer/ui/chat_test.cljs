4f3b68bc7dab07f0913481b5df787f2cc65fb8a4
Paul Lam <paul@quantisan.com>
2026-04-15 08:23:31 +0900

    chat: render compact References row on user messages with excerpts

--- test/gremllm/renderer/ui/chat_test.cljs
+++ test/gremllm/renderer/ui/chat_test.cljs
@@ -1,5 +1,7 @@
 (ns gremllm.renderer.ui.chat-test
   (:require [cljs.test :refer-macros [deftest is testing]]
+            [clojure.string :as str]
+            [clojure.walk :as walk]
             [gremllm.renderer.ui.chat :as chat-ui]
             [lookup.core :as lookup]))
 
@@ -14,3 +16,73 @@
       (is (= [[:effects/prevent-default] [:form.actions/submit]]
              (-> form lookup/attrs (get-in [:on :submit])))
           "The on-submit actions should be correct and in order."))))
+
+(defn- flatten-strings [hiccup]
+  (let [acc (atom [])]
+    (walk/postwalk
+      (fn [x]
+        (when (string? x)
+          (swap! acc conj x))
+        x)
+      hiccup)
+    @acc))
+
+(defn- contains-text? [hiccup s]
+  (some #(str/includes? % s) (flatten-strings hiccup)))
+
+(def same-block-excerpt
+  {:id "e1"
+   :text "this is a selection longer than forty characters abc"
+   :locator {:document-relative-path "document.md"
+             :start-block {:kind :paragraph
+                           :index 3
+                           :start-line 5
+                           :end-line 5
+                           :block-text-snippet "full block text"}
+             :end-block {:kind :paragraph
+                         :index 3
+                         :start-line 5
+                         :end-line 5
+                         :block-text-snippet "full block text"}
+             :start-offset 0
+             :end-offset 52}})
+
+(def cross-block-excerpt
+  {:id "e2"
+   :text "spanning"
+   :locator {:document-relative-path "document.md"
+             :start-block {:kind :heading
+                           :index 1
+                           :start-line 1
+                           :end-line 1
+                           :block-text-snippet "Title"}
+             :end-block {:kind :paragraph
+                         :index 2
+                         :start-line 3
+                         :end-line 3
+                         :block-text-snippet "Body"}}})
+
+(deftest plain-user-message-has-no-references-test
+  (let [hiccup (#'chat-ui/render-message {:id 1 :type :user :text "hello"})]
+    (is (not (contains-text? hiccup "References")))
+    (is (contains-text? hiccup "hello"))))
+
+(deftest user-message-with-same-block-excerpt-renders-compact-pill-test
+  (let [hiccup (#'chat-ui/render-message
+                {:id 1
+                 :type :user
+                 :text "reword these"
+                 :context {:excerpts [same-block-excerpt]}})]
+    (is (contains-text? hiccup "reword these"))
+    (is (contains-text? hiccup "p3"))
+    (is (contains-text? hiccup "this is a selection longer than forty c"))
+    (is (not (contains-text? hiccup "full block text")))
+    (is (not (contains-text? hiccup "this is a selection longer than forty characters abc")))))
+
+(deftest user-message-with-cross-block-excerpt-renders-arrow-label-test
+  (let [hiccup (#'chat-ui/render-message
+                {:id 1
+                 :type :user
+                 :text "compare"
+                 :context {:excerpts [cross-block-excerpt]}})]
+    (is (contains-text? hiccup "h1 -> p2"))))
