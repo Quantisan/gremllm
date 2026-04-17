f0a464c360889b59944026927a4e802c9c4abe3e
Paul Lam <paul@quantisan.com>
2026-04-17 10:07:44 +0900

    test(document): drop leaky excerpt assertions

--- test/gremllm/renderer/actions/document_test.cljs
+++ test/gremllm/renderer/actions/document_test.cljs
@@ -7,10 +7,4 @@
   (let [effects (document/set-content {} "# Replaced")]
     (testing "saves the new content first"
       (is (= [:effects/save document-state/content-path "# Replaced"]
-             (first effects))))
-
-    (testing "dispatches cross-topic excerpt invalidation"
-      (is (some #{[:excerpt.actions/invalidate-across-topics]} effects)))
-
-    (testing "dismisses live capture state"
-      (is (some #{[:excerpt.actions/dismiss-popover]} effects)))))
+             (first effects))))))
