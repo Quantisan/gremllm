9032dfcfd17b592f8aa4a1224ed9853ca94d75f4
Paul Lam <paul@quantisan.com>
2026-04-30 06:19:38 +0900

    chore: refactor out scripts/dev.sh and name each concurrent process for easier read

---
+++ scripts/dev.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# Remove stale build artifacts so wait-on doesn't treat existing files as ready
+rm -f target/main.js resources/public/compiled-js/manifest.edn
+
+concurrently \
+  --names "shadow,electron,dataspex,browser" \
+  --prefix "[{name}]" \
+  --prefix-colors "cyan,yellow,green,magenta" \
+  "shadow-cljs watch main-dev renderer-dev" \
+  "wait-on file:target/main.js file:resources/public/compiled-js/manifest.edn && ELECTRON_IS_DEV=1 electron-forge start" \
+  "clj -A:dev -M dev/start_dataspex.clj" \
+  "wait-on tcp:7117 && open -g http://localhost:7117"
